1284708be2eb2506767d38d73b1e4e449e201bc0
PinkyUni <anya.beliatskaya.751001@gmail.com>
2018-10-31 00:41:23 +0300

    Added navigation menu

--- app/src/main/java/com/example/user/catrunner/MainActivity.java
+++ app/src/main/java/com/example/user/catrunner/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.user.catrunner;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public ImageButton btnHome;
+    public ImageButton btnProfile;
+    public ImageButton btnHistory;
+    public ImageButton btnSettings;
+    public ImageButton btnInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnHistory = findViewById(R.id.btn_history);
+        btnHome = findViewById(R.id.btn_home);
+        btnProfile = findViewById(R.id.btn_profile);
+        btnSettings = findViewById(R.id.btn_settings);
+        btnInfo = findViewById(R.id.btn_info);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                btnHome.setImageDrawable(getResources().getDrawable(R.drawable.ic_home_25dp_selected));
+                Toast.makeText(MainActivity.this, getResources().getText(R.string.kek), Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
