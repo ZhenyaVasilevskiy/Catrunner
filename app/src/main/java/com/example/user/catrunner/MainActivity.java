92401c68fa7c780ebb525b076d5340ff5fc3cd79
PinkyUni <anya.beliatskaya.751001@gmail.com>
2018-11-01 04:59:23 +0300

    Added all fragments without design and MapActivity as well

--- app/src/main/java/com/example/user/catrunner/MainActivity.java
+++ app/src/main/java/com/example/user/catrunner/MainActivity.java
@@ -9,6 +9,9 @@ import android.widget.Toast;
 
 import fragments.HistoryFragment;
 import fragments.HomeFragment;
+import fragments.InfoFragment;
+import fragments.ProfileFragment;
+import fragments.SettingsFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     public HomeFragment homeFragment;
     public HistoryFragment historyFragment;
     public FragmentTransaction fragmentTransaction;
+    public ProfileFragment profileFragment;
+    public SettingsFragment settingsFragment;
+    public InfoFragment infoFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +38,19 @@ public class MainActivity extends AppCompatActivity {
         btnInfo = findViewById(R.id.btn_info);
         homeFragment = new HomeFragment();
         historyFragment = new HistoryFragment();
+        profileFragment = new ProfileFragment();
+        settingsFragment = new SettingsFragment();
+        infoFragment = new InfoFragment();
 //
+        fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.frgmCont, homeFragment);
+        btnHome.setImageDrawable(getResources().getDrawable(R.drawable.ic_home_25dp_selected));
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
         fragmentTransaction = getFragmentManager().beginTransaction();
         fragmentTransaction.add(R.id.frgmCont, homeFragment);
         fragmentTransaction.commit();
@@ -45,10 +63,27 @@ public class MainActivity extends AppCompatActivity {
                 fragmentTransaction.replace(R.id.frgmCont, homeFragment);
                 btnHome.setImageDrawable(getResources().getDrawable(R.drawable.ic_home_25dp_selected));
                 Toast.makeText(MainActivity.this, getResources().getText(R.string.kek), Toast.LENGTH_LONG).show();
+                setTitle(getResources().getString(R.string.app_name));
                 break;
             case R.id.btn_history:
                 btnHome.setImageDrawable(getResources().getDrawable(R.drawable.ic_home_25dp));
                 fragmentTransaction.replace(R.id.frgmCont, historyFragment);
+                setTitle(getResources().getString(R.string.title_history));
+                break;
+            case R.id.btn_profile:
+                btnHome.setImageDrawable(getResources().getDrawable(R.drawable.ic_home_25dp));
+                fragmentTransaction.replace(R.id.frgmCont, profileFragment);
+                setTitle(getResources().getString(R.string.title_profile));
+                break;
+            case R.id.btn_settings:
+                btnHome.setImageDrawable(getResources().getDrawable(R.drawable.ic_home_25dp));
+                fragmentTransaction.replace(R.id.frgmCont, settingsFragment);
+                setTitle(getResources().getString(R.string.title_settings));
+                break;
+            case R.id.btn_info:
+                btnHome.setImageDrawable(getResources().getDrawable(R.drawable.ic_home_25dp));
+                fragmentTransaction.replace(R.id.frgmCont, infoFragment);
+                setTitle(getResources().getString(R.string.title_info));
                 break;
             default:
                 break;
