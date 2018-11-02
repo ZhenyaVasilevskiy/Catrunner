6a64eb875d067aa21f16868991db0b7e45b2325a
PinkyUni <anya.beliatskaya.751001@gmail.com>
2018-11-02 15:31:53 +0300

    Fixed closing

--- app/src/main/java/com/example/user/catrunner/MainActivity.java
+++ app/src/main/java/com/example/user/catrunner/MainActivity.java
@@ -54,7 +54,6 @@ public class MainActivity extends AppCompatActivity {
             case R.id.btn_home:
                 fragmentTransaction.replace(R.id.frgmCont, homeFragment);
                 btnHome.setImageDrawable(getResources().getDrawable(R.drawable.ic_home_25dp_selected));
-                Toast.makeText(MainActivity.this, getResources().getText(R.string.kek), Toast.LENGTH_LONG).show();
                 setTitle(getResources().getString(R.string.app_name));
                 break;
             case R.id.btn_history:
