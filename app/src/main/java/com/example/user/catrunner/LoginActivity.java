fd6d662293bc817088e5d81d912d8cc141eef03e
PinkyUni <anya.beliatskaya.751001@gmail.com>
2018-11-15 20:09:03 +0300

    Added styles, changed colors

--- app/src/main/java/com/example/user/catrunner/LoginActivity.java
+++ app/src/main/java/com/example/user/catrunner/LoginActivity.java
@@ -4,17 +4,33 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
+import android.text.TextWatcher;
 import android.text.style.ForegroundColorSpan;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private class User {
+        private String name, password;
+
+        public User(String name, String password) {
+            this.name = name;
+            this.password = password;
+        }
+    }
+
     public static Typeface typefaceBebasNeue, typefaceOpenSansBold, typefaceOpenSansRegular;
-    public Button btnSignIn, btnCreateAccount;
+    public Button btnLogin, btnCreateAccount;
+    public android.support.design.widget.TextInputEditText edtPassword, edtLogin;
+    public ArrayList<User> users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,16 +48,79 @@ public class LoginActivity extends AppCompatActivity {
         helloText.setText(text);
         helloText.setTypeface(typefaceOpenSansBold);
 
-        btnSignIn = findViewById(R.id.btn_sign_in);
-        btnSignIn.setTypeface(typefaceOpenSansBold);
-        btnSignIn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                startActivity(intent);
-            }
-        });
+        edtPassword = findViewById(R.id.password);
+        edtLogin = findViewById(R.id.login);
+        btnLogin = findViewById(R.id.btn_login);
+        btnLogin.setTypeface(typefaceOpenSansBold);
         btnCreateAccount = findViewById(R.id.btn_create_account);
         btnCreateAccount.setTypeface(typefaceOpenSansBold);
+
+        users = new ArrayList<>();
+        users.add(new User("cat", "runner"));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User user = new User(edtLogin.getText().toString(), edtPassword.getText().toString());
+                boolean check = false;
+                for (User i : users) {
+                    if (i.password.equals(user.password) && i.name.equals(user.name)) {
+                        check = true;
+                    }
+                }
+                if (check) {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Such user doesn't exists", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnCreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                User user = new User(edtLogin.getText().toString(), edtPassword.getText().toString());
+                boolean check = false;
+                for (User i : users) {
+                    if (i.password.equals(user.password) && i.name.equals(user.name)) {
+                        check = true;
+                    }
+                }
+                if (check) {
+                    Toast.makeText(LoginActivity.this, "Such user already exists", Toast.LENGTH_SHORT).show();
+                } else {
+                    users.add(user);
+                    Toast.makeText(LoginActivity.this, "Account created", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        TextWatcher txt = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (edtPassword.getText().length() != 0) {
+                    btnLogin.setEnabled(true);
+                } else {
+                    btnLogin.setEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+        edtPassword.addTextChangedListener(txt);
     }
 }
